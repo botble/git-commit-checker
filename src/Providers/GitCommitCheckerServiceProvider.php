336b7f45e513e4f4a13a043fe9e18565f45969bb
Dinh Quoc Han <dqh@dinhquochan.com>
2023-01-05 15:17:24 +0700

    [2.x] Switch to use Laravel Pint
    Co-Authored-By: Sang Nguyen <minhsang2603@gmail.com>

--- src/Providers/GitCommitCheckerServiceProvider.php
+++ src/Providers/GitCommitCheckerServiceProvider.php
@@ -6,14 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class GitCommitCheckerServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/git-commit-checker.php',
+            'git-commit-checker'
+        );
+    }
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/git-commit-checker.php', 'git-commit-checker');
+        $this->app->register(CommandServiceProvider::class);
 
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../../config/git-commit-checker.php' => config_path('git-commit-checker.php')], 'config');
+            $this->publishes([
+                __DIR__ . '/../../config/git-commit-checker.php' => config_path('git-commit-checker.php'),
+            ], 'git-commit-checker-config');
         }
-
-        $this->app->register(CommandServiceProvider::class);
     }
 }
